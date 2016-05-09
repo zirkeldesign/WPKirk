524420dc39e2bfdb164a52c3cdfe2f1ffbacf401
Giovambattista (John) Fazioli <john.fazioli@namecheap.com>
2016-05-09 17:32:43 +0200

    Added options demo view

---
+++ resources/views/dashboard/options.php
@@ -0,0 +1,83 @@
+<!--
+ |
+ | In $plugin you'll find an instance of Plugin class.
+ | If you'd like can pass variable to this view, for example:
+ |
+ | return PluginClassName()->view( 'dashboard.index', [ 'var' => 'value' ] );
+ |
+-->
+
+<div class="kirk">
+  <h1>Options</h1>
+  <p>The current options are:</p>
+  <p><code> echo $plugin->options</code></p>
+
+  <pre><?php echo $plugin->options ?></pre>
+
+
+  <p>Get option <code>echo $plugin->options->get( 'General.option_2')
+      = <?php echo $plugin->options->get( 'General.option_2' ) ?></code></p>
+  <p>Get branch option <code>echo $plugin->options->get( 'General.option_3')
+      = <?php echo var_export( $plugin->options->get( 'General.option_3' ) ) ?></code></p>
+  <p>Get option <code>echo $plugin->options->get( 'General.option_3.sub_option_of_3')
+      = <?php echo $plugin->options->get( 'General.option_3.sub_option_of_3' ) ?></code></p>
+  <p>Get default option <code>echo $plugin->options->get( 'General.doNotExists', 'default' )
+      = <?php echo $plugin->options->get( 'General.doNotExists', 'default' ) ?></code></p>
+
+  <h2>Update</h2>
+  <p>Change <code>$plugin->options->set( 'Special.Name', 'John' );</code></p>
+  <?php $plugin->options->set( 'Special.Name', 'John' ) ?>
+  <pre><?php echo $plugin->options ?></pre>
+
+  <p>Change with mixed value <code>$plugin->options->set( 'Special.Name', [ 'John', 'Good' ] );</code></p>
+  <?php $plugin->options->set( 'Special.Name', [ 'John', 'Good' ] ) ?>
+  <pre><?php echo $plugin->options ?></pre>
+
+  <p>Add <code>$plugin->options->set( 'Special.time', time() );</code></p>
+  <?php $plugin->options->set( 'Special.time', time() ) ?>
+  <pre><?php echo $plugin->options ?></pre>
+
+  <p>Mass update <code>$plugin->options->update( [ 'General' => [ 'option_4' => [ 'color' => 'red', 'background' => 'transparent' ] ] ] );</code></p>
+  <?php $plugin->options->update(
+    [
+      'General' => [
+        'option_4' => [
+          'color'      => 'red',
+          'background' => 'transparent'
+        ]
+      ]
+    ]
+  )
+  ?>
+  <pre><?php echo $plugin->options ?></pre>
+
+  <p>Mass insert <code>$plugin->options->update( [ 'General' => [ 'option_5' => [ 'color' => 'red', 'background' => 'transparent' ] ] ] );</code></p>
+  <?php $plugin->options->update(
+    [
+      'General' => [
+        'option_5' => [
+          'color'      => 'red',
+          'background' => 'transparent'
+        ]
+      ]
+    ]
+  )
+  ?>
+  <pre><?php echo $plugin->options ?></pre>
+
+  <h2>Delete</h2>
+  <p>Delete option <code>$plugin->options->delete( 'General.option_4' )</code></p>
+  <?php $plugin->options->delete( 'General.option_4' ) ?>
+  <pre><?php echo $plugin->options ?></pre>
+
+  <h2>Delete all</h2>
+  <p>Delete option <code>$plugin->options->delete()</code></p>
+  <?php $plugin->options->delete() ?>
+  <pre><?php echo $plugin->options ?></pre>
+
+  <h2>Reset to default</h2>
+  <p><code>$plugin->options->reset()</code></p>
+  <?php $plugin->options->reset() ?>
+  <pre><?php echo $plugin->options ?></pre>
+
+</div>
