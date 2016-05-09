531aa1a47c5d0ffb211499015509c3541d395f08
Giovambattista (John) Fazioli <john.fazioli@namecheap.com>
2016-05-09 17:41:34 +0200

    Added options demo view

--- plugin/Http/Controllers/Dashboard/DashboardController.php
+++ plugin/Http/Controllers/Dashboard/DashboardController.php
@@ -6,18 +6,21 @@ use WPKirk\Http\Controllers\Controller;
 
 class DashboardController extends Controller {
 
-  //
   public function firstMenu()
   {
     return WPKirk()->view( 'dashboard.index' )->with( 'kirk', 'Captain' );
   }
 
-  //
   public function secondMenu()
   {
     return WPKirk()->view( 'dashboard.second' )
                    ->styles( 'common' )
                    ->footerScripts( 'main' );
+  }
+
+  public function optionsMenu()
+  {
+    return WPKirk()->view( 'dashboard.options' );
   }
 
 }
