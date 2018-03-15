7f91c1240b68a63ebced833763efbd85be9b4960
gfazioli <giovambattista.fazioli@gmail.com>
2018-03-15 18:16:30 +0100

    update version

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Plugin Name: WP Kirk
  * Plugin URI: http://undolog.com
  * Description: WordPress plugin demo bases on WP Bones framework
- * Version: 0.8.36
+ * Version: 0.8.37
  * Author: Giovambattista Fazioli
  * Author URI: http://undolog.com
  * Text Domain: wp-kirk
